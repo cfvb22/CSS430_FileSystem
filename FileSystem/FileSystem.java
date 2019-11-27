f684f31d4759a846840d2ca85b2392d7d812c99b
Camila Valdebenito <cfvb22@uw.edu>
2019-11-27 01:32:03 -0800

    Create FileSystem.java

---
+++ FileSystem/FileSystem.java
@@ -0,0 +1,30 @@
+public class FileSystem {
+  private SuperBlock superblock;
+  private Directory directory;
+  private FileTable filetable;
+  
+  public FileSystem( int diskBlock ) {
+    // create superblock and format disk with 64 inodes in default
+    superblock = new SuperBlock( diskBlocks );
+    
+    // creat directory and register "/" in directory entry 0
+    directory = new Directory( superblock.inodeBlocks );
+    
+    // file table is created and stores directory in the file table
+    filetable = new FileTable( directory );
+    
+    // directory reconstruction
+    FileTableEntry dirEnt = open( "/", "r" );
+    int directorySize = fsize( dirEnt );
+    if( dirSize > 0) {
+      byte[] dirData = new byte[directorySize];
+      read( dirEnt, dirData );
+      directory.bytes2directory( dirData );
+    }
+    close( dirEnt ); 
+  }
+  
+
+
+
+}
